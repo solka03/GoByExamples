ef2da80b05d2eab14b31ebe8e5a23d1d9b71c7b3
kamlesh <kamlesh.ldce@gmail.com>
2017-07-12 19:31:04 +0530

    Select & Timeout

--- Timeouts.go
+++ Timeouts.go
@@ -1,3 +1,20 @@
 package main
 
+import (
+	"time"
+	"fmt"
+)
 
+func main()  {
+
+	c1 := make(chan string,1)
+
+	go func() {
+
+		time.Sleep(time.Second *1)
+		c1 <-"one"
+
+	}()
+
+	fmt.Println("channel : ",<-c1)
+}
