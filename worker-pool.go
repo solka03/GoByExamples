efa6e889a5470a2ae502e115cd79947d498e7df2
kamlesh <kamlesh.ldce@gmail.com>
2017-07-13 17:12:32 +0530

    Mutex, worker pool

--- worker-pool.go
+++ worker-pool.go
@@ -1,3 +1,37 @@
 package main
 
+import (
+	"fmt"
+	"time"
+)
+
+func workers(id int, jobs <-chan int,results chan<- int)  {
+	for j := range jobs {
+		fmt.Println("worker", id, "started  job", j)
+		time.Sleep(time.Second)
+		fmt.Println("worker", id, "finished job", j)
+		results <- j * 2
+	}
+}
+
+func main()  {
+
+	jobs := make(chan int,100)
+	results := make(chan int,100)
+
+	for w:=1; w<=3 ; w++ {
+		go workers(w,jobs,results)
+	}
+
+	for j :=1; j <= 5 ; j++ {
+		jobs <- j
+	}
+
+	close(jobs)
+
+	for i :=1 ; i <= 5 ; i++  {
+		<-results
+	}
+}
+
 
